7e8d312f684149620cb0b5ae322abc1e764c8fd8
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-04-29 12:07:27 +0200

    feat: Tetris Game is Now Fully Functional
    - Added highscores board, allowing players to view and track their scores across multiple games, with saving to localStorage and reset functionality.
    - Implemented levels and score handlers, providing players with increasing difficulty and rewarding higher scores for more challenging gameplay.
    - Finished collision checks.
    - Implemented game over handler, triggering the end of the game when a tetromino reaches the top of the game board.
    
    This commit marks the completion of all major functionality in the Tetris game, bringing it to a stable and fully functional state. Still, huge code refactor to do!

--- src/components/TetrisApp/TetrisApp.jsx
+++ src/components/TetrisApp/TetrisApp.jsx
@@ -1,4 +1,5 @@
 import "./src/styles/tetrisMain.css";
+import { pauseIcon } from "./src/icons";
 import { randomTetrominoSequence } from "./src/helpers";
 import React, { useState, useEffect } from "react";
 
@@ -12,6 +13,7 @@ const TetrisApp = () => {
   const [score, setScore] = useState(232332);
   const [gameOver, setGameOver] = useState(false);
   const [level, setLevel] = useState(1);
+  const [lines, setLines] = useState(0);
   const [tetrisBoard, setTetrisBoard] = useState(
     Array.from({ length: 22 }, () => Array.from({ length: 10 }, () => "e"))
   );
@@ -23,19 +25,28 @@ const TetrisApp = () => {
   const [actualTetrominoCol, setActualTetrominoCol] = useState(3);
   const [hasMovedDown, setHasMovedDown] = useState(false);
 
+  const setHighscoresHandler = (score) => {
+    setHighscores((oldVal) => [...oldVal, score]);
+  };
+
+  const resetHighscoresHandler = () => {
+    setHighscores([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  };
+
   const rotateTetromino = () => {
     let blocked = false;
     let rotation = actualTetrominoRotation;
     let tetrominoToCheck;
+
     if (actualTetrominoRotation < 3) rotation += 1;
     if (actualTetrominoRotation === 3) rotation = 0;
-    tetrominoToCheck = selectedTetrominoSequence[rotation];
 
-    tetrominoToCheck?.some((rows, rowsIndex) => {
-      rows.some((col, colIndex) => {
+    tetrominoToCheck = selectedTetrominoSequence[rotation];
+    tetrominoToCheck.forEach((rows, rowsIndex) => {
+      rows.forEach((col, colIndex) => {
         if (col === "t") {
           if (
-            rowsIndex + actualTetrominoRow >= 21 ||
+            rowsIndex + actualTetrominoRow >= 23 ||
             colIndex + actualTetrominoCol > 9 ||
             colIndex + actualTetrominoCol < 0 ||
             tetrisBoard[rowsIndex + actualTetrominoRow][
@@ -60,6 +71,7 @@ const TetrisApp = () => {
     setActualTetrominoRotation(0);
     setActualTetrominoRow(0);
     setActualTetrominoCol(3);
+    setActualTetromino(selectedTetrominoSequence[actualTetrominoRotation]);
   };
 
   const transformTetrominoToBlock = () => {
@@ -72,25 +84,23 @@ const TetrisApp = () => {
         }
       })
     );
-    placeTetrominoOnBoard();
-    setTetrisBoard(transformedTetrisBoard);
+
+    const removedBoard = removeBlockLineFromBoard(transformedTetrisBoard);
+    setTetrisBoard(removedBoard);
   };
 
-  const removeBlockLineFromBoard = () => {
-    let linesRemovedCounter = 0;
-    const copiedTetrisBoard = [...tetrisBoard];
-
-    tetrisBoard?.forEach((row, rowsIndex) => {
+  const removeBlockLineFromBoard = (board) => {
+    const copiedTetrisBoard = [...board];
+    board.forEach((row, rowsIndex) => {
       if (row.every((item) => item === "b")) {
-        linesRemovedCounter += 1;
+        setScore((oldVal) => oldVal + level * 100);
         copiedTetrisBoard.splice(rowsIndex, 1);
+        copiedTetrisBoard.unshift(Array.from({ length: 10 }, () => "e"));
+        setLines((oldVal) => oldVal + 1);
       }
     });
 
-    for (let i = 0; i < linesRemovedCounter; i++)
-      copiedTetrisBoard.unshift(Array.from({ length: 10 }, () => "e"));
-
-    setTetrisBoard(copiedTetrisBoard);
+    return copiedTetrisBoard;
   };
 
   const moveTetrominoDown = () => {
@@ -99,70 +109,73 @@ const TetrisApp = () => {
   };
 
   const moveTetrominoRight = () => {
+    let blocked = false;
+
+    actualTetromino?.forEach((rows, rowsIndex) => {
+      rows.forEach((col, colIndex) => {
+        if (col === "t")
+          if (
+            tetrisBoard[rowsIndex + actualTetrominoRow][
+              colIndex + actualTetrominoCol + 1
+            ] === "b" ||
+            colIndex + actualTetrominoCol + 1 === 10
+          ) {
+            blocked = true;
+          }
+      });
+    });
+
+    if (blocked) return;
+
     setActualTetrominoCol((oldVal) => (oldVal += 1));
   };
 
   const moveTetrominoLeft = () => {
+    let blocked = false;
+
+    actualTetromino?.forEach((rows, rowsIndex) => {
+      rows.forEach((col, colIndex) => {
+        if (col === "t")
+          if (
+            tetrisBoard[rowsIndex + actualTetrominoRow][
+              colIndex + actualTetrominoCol - 1
+            ] === "b" ||
+            colIndex + actualTetrominoCol - 1 === -1
+          )
+            blocked = true;
+      });
+    });
+
+    if (blocked) return;
+
     setActualTetrominoCol((oldVal) => (oldVal -= 1));
   };
 
   const renderTetromino = () => {
-    const tetrominoColumnMove = actualTetrominoCol;
     let blocked = false;
 
     if (hasMovedDown) {
       actualTetromino?.some((rows, rowsIndex) => {
         rows.some((col, colIndex) => {
-          if (col === "t") {
-            if (
-              rowsIndex + actualTetrominoRow === 22 ||
-              tetrisBoard[rowsIndex + actualTetrominoRow][
-                colIndex + actualTetrominoCol
-              ] === "b"
-            ) {
-              blocked = true;
-              transformTetrominoToBlock();
-              placeTetrominoOnBoard();
-            }
+          if (col !== "t") return false;
+          if (
+            rowsIndex + actualTetrominoRow === 22 ||
+            tetrisBoard[rowsIndex + actualTetrominoRow][
+              colIndex + actualTetrominoCol
+            ] === "b"
+          ) {
+            blocked = true;
+            transformTetrominoToBlock();
+            placeTetrominoOnBoard();
+            return true;
           }
+          return false;
         });
+        if (blocked) return true;
+        return false;
       });
       setHasMovedDown(false);
     }
-
-    if (blocked) return;
-
-    actualTetromino?.some((rows, rowsIndex) => {
-      rows.some((col, colIndex) => {
-        if (col === "t")
-          if (
-            tetrisBoard[rowsIndex + actualTetrominoRow][
-              colIndex + actualTetrominoCol
-            ] === "b" ||
-            colIndex + actualTetrominoCol === -1
-          ) {
-            blocked = true;
-            setActualTetrominoCol(tetrominoColumnMove + 1);
-          }
-      });
-    });
-
-    if (blocked) return;
-
-    actualTetromino?.some((rows, rowsIndex) => {
-      rows.forEach((col, colIndex) => {
-        if (col === "t")
-          if (
-            tetrisBoard[rowsIndex + actualTetrominoRow][
-              colIndex + actualTetrominoCol
-            ] === "b" ||
-            colIndex + actualTetrominoCol === 10
-          ) {
-            blocked = true;
-            setActualTetrominoCol(tetrominoColumnMove - 1);
-          }
-      });
-    });
 
     if (blocked) return;
 
@@ -181,25 +194,35 @@ const TetrisApp = () => {
           ] = col;
       });
     });
+
     setTetrisBoard(copiedTetrisBoard);
   };
 
-  const gameOverCheck = () => {
+  const gameOverCheckHandler = () => {
     tetrisBoard?.some((row, rowIndex) => {
       row.some((colItem) => {
-        if (rowIndex <= 4 && colItem === "b") {
+        if (rowIndex <= 1 && colItem === "b") {
           setGameOver(true);
+          setPauseGame(true);
+          setResumeGame(false);
           return true;
         }
         return false;
       });
+      return false;
     });
   };
 
   const startGameHandler = () => {
+    setLevel(1);
+    setLines(0);
+    setTetrisBoard(
+      Array.from({ length: 22 }, () => Array.from({ length: 10 }, () => "e"))
+    );
     setPauseGame(false);
     placeTetrominoOnBoard();
     setScore(0);
+    setGameOver(false);
   };
 
   const pauseGameHandler = () => {
@@ -239,6 +262,23 @@ const TetrisApp = () => {
     }
   };
 
+  // import highscores from local storage, if available, at start of the component
+  useEffect(() => {
+    const savedTetrisHighscores = localStorage.getItem("savedTetrisHighscores");
+    if (savedTetrisHighscores) {
+      setHighscores(JSON.parse(savedTetrisHighscores));
+    }
+  }, []);
+
+  // save highscores to local storage, each time new scores is added to highscores
+  useEffect(() => {
+    localStorage.setItem("savedTetrisHighscores", JSON.stringify(highscores));
+  }, [highscores]);
+
+  useEffect(() => {
+    if (gameOver) setHighscoresHandler(score);
+  }, [gameOver, score]);
+
   useEffect(() => {
     function keyPress(event) {
       if (!pauseGame) handleKeyPress(event);
@@ -247,6 +287,7 @@ const TetrisApp = () => {
     return () => {
       document.removeEventListener("keydown", keyPress);
     };
+    // eslint-disable-next-line
   }, [
     pauseGame,
     actualTetromino,
@@ -254,31 +295,57 @@ const TetrisApp = () => {
     actualTetrominoRow,
     actualTetrominoRotation,
     selectedTetrominoSequence,
+    lines,
   ]);
 
-  useEffect(() => {
-    setActualTetromino(selectedTetrominoSequence[actualTetrominoRotation]);
-    renderTetromino();
-  }, [
-    actualTetromino,
-    actualTetrominoCol,
-    actualTetrominoRow,
-    actualTetrominoRotation,
-    selectedTetrominoSequence,
-  ]);
+  useEffect(
+    () => {
+      setActualTetromino(selectedTetrominoSequence[actualTetrominoRotation]);
+      renderTetromino();
+      gameOverCheckHandler();
+    },
+    // eslint-disable-next-line
+    [
+      actualTetromino,
+      actualTetrominoCol,
+      actualTetrominoRow,
+      actualTetrominoRotation,
+      selectedTetrominoSequence,
+      lines,
+    ]
+  );
+  useEffect(
+    () => {
+      let interval;
+      if (!pauseGame) {
+        interval = window.setInterval(() => {
+          moveTetrominoDown();
+        }, 1000 * (1 / level));
+      }
+
+      return () => {
+        window.clearInterval(interval);
+      };
+    },
+    // eslint-disable-next-line
+    [
+      pauseGame,
+      actualTetromino,
+      actualTetrominoCol,
+      actualTetrominoRow,
+      actualTetrominoRotation,
+      selectedTetrominoSequence,
+      lines,
+    ]
+  );
 
   useEffect(() => {
-    let interval;
-    if (!pauseGame) {
-      interval = window.setInterval(() => {
-        moveTetrominoDown();
-      }, 1000 * (1 / level));
-    }
-
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, [pauseGame]);
+    if (lines > 10) setLevel(2);
+    if (lines > 20) setLevel(3);
+    if (lines > 30) setLevel(4);
+    if (lines > 40) setLevel(5);
+    if (lines > 50) setLevel(6);
+  }, [lines, level]);
 
   return (
     <div className="tetris">
@@ -299,6 +366,7 @@ const TetrisApp = () => {
                     onClick={() => {
                       setShowHighscore(true);
                       setShowMenu(false);
+                      setGameOver(false);
                     }}
                   >
                     Highscores
@@ -307,32 +375,49 @@ const TetrisApp = () => {
                     onClick={() => {
                       setShowHelp(true);
                       setShowMenu(false);
+                      setGameOver(false);
                     }}
                   >
                     Help
                   </button>
-
-                  <button onClick={() => {}}>Quit</button>
                 </div>
               ) : (
                 ""
               )}
-
+              {gameOver ? (
+                <div className="tetris__game-board__game-over">
+                  <p>Game Over!</p>
+                  <p>Your score:</p>
+                  <p>{score}</p>
+                  <p>Highscore:</p>
+                  <p>{highscores.sort((a, b) => b - a)[0]}</p>
+                </div>
+              ) : (
+                ""
+              )}
               {showHighscore ? (
                 <div className="tetris__game-board__menu__highscores">
                   <div>Highscores</div>
+                  <div>
+                    {highscores
+                      .sort((a, b) => {
+                        return b - a;
+                      })
+                      .slice(0, 8)
+                      .map((item, index) => (
+                        <div key={index * Math.random()}>
+                          <p>{index + 1}.</p>
+                          <p>{item}</p>
+                        </div>
+                      ))}
+                  </div>
 
-                  {highscores
-                    .sort((a, b) => {
-                      return b - a;
-                    })
-                    .slice(0, 8)
-                    .map((item, index) => (
-                      <div key={index * Math.random()}>
-                        <p>{index + 1}.</p>
-                        <p>{item}</p>
-                      </div>
-                    ))}
+                  <button
+                    onClick={resetHighscoresHandler}
+                    className="tetris-reset"
+                  >
+                    Reset
+                  </button>
                   <button
                     onClick={() => {
                       setShowMenu(true);
@@ -370,7 +455,7 @@ const TetrisApp = () => {
           ) : (
             <div className="tetris__game-board-container">
               {tetrisBoard.map((rows, rowsIndex) => {
-                if (rowsIndex <= 1) return;
+                if (rowsIndex <= 1) return "";
                 return rows.map((item, colIndex) => (
                   <div
                     key={`indexKey${rowsIndex}${colIndex}`}
@@ -386,21 +471,23 @@ const TetrisApp = () => {
         </div>
 
         <div className="tetris__score-board">
+          {!pauseGame ? (
+            <button onClick={pauseGameHandler}>
+              <img className="tetris-pause" src={pauseIcon} alt="pause" />
+            </button>
+          ) : (
+            ""
+          )}
           <div>
             <p>Lines</p>
-            <p>1/30</p>
-          </div>
-          <div>Loading...</div>
-          <button onClick={rotateTetromino}>rotate</button>
-          <button onClick={transformTetrominoToBlock}>freeze</button>
-          <button onClick={removeBlockLineFromBoard}>removeLine</button>
-          <button onClick={gameOverCheck}>gameOverCheck</button>
-          <div>
-            <p>Time:</p>
-            <p>01:25</p>
+            <p>{lines}</p>
           </div>
           <div>
-            <p>Score:</p>
+            <p>Level</p>
+            <p>{level}/6</p>
+          </div>
+          <div>
+            <p>Score</p>
             <p>{score}</p>
           </div>
         </div>
---
+++ src/components/TetrisApp/src/icons/index.js
@@ -0,0 +1,3 @@
+import pauseIcon from "./pause-icon.svg";
+
+export { pauseIcon };
