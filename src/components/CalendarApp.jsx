52e10432669679e375d2c959d49d57609d79ba50
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-07-17 14:21:30 +0200

    fix: changed allTasks object structure in CalendarApp for easier managment

--- src/components/CalendarApp.jsx
+++ src/components/CalendarApp.jsx
@@ -4,7 +4,7 @@ import "../assets/styles/calendarApp.css";
 import { WholeAppContext } from "../App";
 
 //for testing
-const tasksToDo = {};
+const tasksToDo = [];
 
 const weekdays = [
   "Monday",
@@ -27,22 +27,55 @@ const CalendarApp = () => {
   const [allTasks, setAllTasks] = useState(tasksToDo);
 
   const [date, setDate] = useState(new Date());
-  const [inputValue, setInputValue] = useState();
+
+  const [startInputValue, setStartInputValue] = useState();
+  const [endInputValue, setEndInputValue] = useState();
+
   const [showAddTask, setShowAddTask] = useState(false);
-  const [topic, setTopic] = useState("");
+  const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
   const addNewTask = () => {
-    const keyVal = date.toLocaleDateString();
-    const id = Math.floor(Math.random() * 100000000);
-    setAllTasks((oldVal) => ({
+    const startTaskDate = new Date(startInputValue);
+    startTaskDate.setHours(0);
+    const endTaskDate = new Date(endInputValue);
+    endTaskDate.setHours(0);
+
+    // for easier testing
+    // const id = Math.floor(Math.random() * 100000000);
+    const id = 2;
+
+    setAllTasks((oldVal) => [
       ...oldVal,
-      [keyVal]: [...(oldVal[keyVal] || []), { id, title: topic, description }],
-    }));
+      { id, startTaskDate, endTaskDate, title, description },
+    ]);
+  };
+
+  const removeTask = (id) => {
+    const updatedTasks = allTasks.filter((task) => task.id !== id);
+    setAllTasks(updatedTasks);
   };
 
   const actualMonth = () => {
-    setDate(new Date());
+    const actualDate = new Date();
+    actualDate.setDate(1);
+    setDate(actualDate);
+  };
+
+  const prevYear = () => {
+    setDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setFullYear(newDate.getFullYear() - 1);
+      return newDate;
+    });
+  };
+
+  const nextYear = () => {
+    setDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setFullYear(newDate.getFullYear() + 1);
+      return newDate;
+    });
   };
 
   const prevMonth = () => {
@@ -71,31 +104,41 @@ const CalendarApp = () => {
 
     // Add empty cells for previous month's days
     for (let i = 0; i < firstDayOfWeek; i++) {
-      calendarDays.push(<div className="calendar--app--main__day empty"></div>);
+      calendarDays.push(
+        <div
+          key={`empty-${i}`}
+          className="calendar--app--main__day empty"
+        ></div>
+      );
     }
 
     // Add cells for current month's days
     for (let day = 1; day <= daysInMonth; day++) {
-      const targetDate = new Date(year, month, day).toLocaleDateString("pl");
-      console.log(targetDate);
-      let list = [];
-      if (allTasks.hasOwnProperty(targetDate)) {
-        list.push(...allTasks[targetDate]);
-      }
+      const targetDate = new Date(year, month, day);
+
+      // create list of tasks for each day
+      const list = allTasks.filter(
+        (task) =>
+          task.startTaskDate <= targetDate && task.endTaskDate >= targetDate
+      );
 
       calendarDays.push(
         <div
+          key={`day-${day}`}
           className={`calendar--app--main__day ${
             day === date.getDate() ? "active--day" : ""
           }`}
           onClick={() => {
-            setDate(new Date(year, month, day));
+            const selectedDate = new Date(year, month, day);
+            setDate(selectedDate);
+            setStartInputValue(selectedDate.toISOString().substring(0, 10));
+            setEndInputValue(selectedDate.toISOString().substring(0, 10));
           }}
         >
           {day}
           <div className="calendar--app--main__day__tasks--list">
-            {list?.map((item) => (
-              <p>{item.title}</p>
+            {list?.map((item, index) => (
+              <p key={`${index}${item.id}`}>{item.title}</p>
             ))}
           </div>
         </div>
@@ -109,33 +152,63 @@ const CalendarApp = () => {
       <header>
         <p>Today: {today.toLocaleDateString(lang.lng)}</p>
       </header>
+
       <aside>
         <ul>
           <li>
             <button onClick={() => setShowAddTask((oldVal) => !oldVal)}>
               Add new task
             </button>
+            <button
+              onClick={() => {
+                console.log(allTasks);
+                console.log(startInputValue, endInputValue);
+              }}
+            >
+              test
+            </button>
+            <button onClick={() => removeTask(2)}>Remove</button>
           </li>
         </ul>
+
         {showAddTask && (
           <form
             className="calendar--app--task"
             onSubmit={(e) => {
               e.preventDefault();
               addNewTask();
-              setTopic("");
+              setTitle("");
               setDescription("");
+              setStartInputValue();
+              setEndInputValue();
               setShowAddTask(false);
             }}
           >
-            <label>Topic:</label>
+            <label htmlFor="startDate">Start:</label>
             <input
+              id="startDate"
+              type="date"
+              value={startInputValue}
+              onChange={(e) => setStartInputValue(e.target.value)}
+            />
+            <label htmlFor="endDate">End:</label>
+            <input
+              id="endDate"
+              type="date"
+              value={endInputValue}
+              onChange={(e) => setEndInputValue(e.target.value)}
+            />
+            <label htmlFor="title">title:</label>
+            <input
+              id="title"
               type="text"
               required
-              value={topic}
-              onChange={(e) => setTopic(e.target.value)}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
+            <label htmlFor="description">Description:</label>
             <textarea
+              id="description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             ></textarea>
@@ -143,34 +216,31 @@ const CalendarApp = () => {
           </form>
         )}
       </aside>
+
       <main>
-        <div className="calendar--app--main">
-          <div className="calendar--app--main__header">
-            Date picker:
-            <input
-              type="date"
-              value={inputValue}
-              onChange={(val) => {
-                setInputValue(val.target.value);
-                setDate(new Date(val.target.value));
-              }}
-            />
-            <button onClick={prevMonth}>Previous Month</button>
-            <button onClick={actualMonth}>Actual Month</button>
-            <button onClick={nextMonth}>Next Month</button>
-            <p>{date.toLocaleDateString(lang.lng)}</p>
-          </div>
-          <div className="calendar--app--main__body">
-            {weekdays.map((day, index) => (
-              <div
-                key={`weekdays${day}+${index}`}
-                className="calendar--app--main__day--name"
-              >
-                {day}
-              </div>
-            ))}
-            {renderCalendar()}
-          </div>
+        <div className="calendar--app--main__header">
+          <p>
+            {date.toLocaleDateString(lang.lng, {
+              year: "numeric",
+              month: "long",
+            })}
+          </p>
+          <button onClick={prevYear}>Previous Year</button>
+          <button onClick={prevMonth}>Previous Month</button>
+          <button onClick={actualMonth}>Actual Month</button>
+          <button onClick={nextMonth}>Next Month</button>
+          <button onClick={nextYear}>Next Year</button>
+        </div>
+        <div className="calendar--app--main__body">
+          {weekdays.map((day, index) => (
+            <div
+              key={`weekdays${day}+${index}`}
+              className="calendar--app--main__day--name"
+            >
+              {day}
+            </div>
+          ))}
+          {renderCalendar()}
         </div>
       </main>
     </div>
