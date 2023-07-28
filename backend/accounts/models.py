66601c08c2662e2bcf3bd553dcd7f5b27bec2b8f
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-07-28 23:04:21 +0200

    feat: Remove session authentication

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,7 +6,7 @@ from django.db.models import JSONField
 from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
-)  ## A new class is imported. ##
+)
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -166,7 +166,6 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
 }
