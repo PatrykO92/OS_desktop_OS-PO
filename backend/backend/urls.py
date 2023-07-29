9ec9a305074eda140272efe5c239eae63b703da8
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-07-29 20:30:53 +0200

    feat: Add URL pattern for serving avatars

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
 
 urlpatterns = [
@@ -37,3 +39,5 @@ urlpatterns = [
         name="account_email_verification_sent",
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
