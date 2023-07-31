bde526693323c017c02c9971efaab038892201c5
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-07-31 09:37:26 +0200

    refactor: Change News API endpoint to use POST method

--- backend/news_api/views.py
+++ backend/news_api/views.py
@@ -16,19 +16,16 @@ class NewsAPIEndpoint(APIView):
     serializer_class = NewsSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        # Define default values for optional query parameters
+    def post(self, request):
         country = "us"
         category = None
-        page_size = 5  # Default page size
+        page_size = 5
 
-        # Override default values with query parameters if they are present
-        if "country" in request.query_params:
-            country = request.query_params["country"]
-        if "category" in request.query_params:
-            category = request.query_params["category"]
+        if "country" in request.data:
+            country = request.data["country"]
+        if "category" in request.data:
+            category = request.data["category"]
 
-        # Make the request to NewsAPI with the optional parameters
         url = "https://newsapi.org/v2/top-headlines"
         params = {"country": country, "apiKey": NEWS_API_KEY, "pageSize": page_size}
         if category:
@@ -36,5 +33,4 @@ class NewsAPIEndpoint(APIView):
 
         response = requests.get(url, params=params)
 
-        # Return the response from NewsAPI
         return Response(response.json())
