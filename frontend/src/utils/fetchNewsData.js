9c38cbb3773057ede5c71e3121180d6358433bc0
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-07-31 11:05:41 +0200

    refactor: Change News API call to use POST method and add authentication

--- frontend/src/utils/fetchNewsData.js
+++ frontend/src/utils/fetchNewsData.js
@@ -3,13 +3,28 @@ import axios from "axios";
 const apiUrl = process.env.REACT_APP_BACKEND_URL;
 
 const fetchNewsData = async (country, category) => {
+  const token = localStorage.getItem("authToken");
+  const config = {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  };
+
   try {
-    const response = await axios.get(
-      `${apiUrl}api/v1/news/get?country=${country}&category=${category}`
+    const requestData = {
+      country,
+      category,
+    };
+
+    const response = await axios.post(
+      `${apiUrl}api/v1/news/`,
+      requestData,
+      config
     );
     return response;
   } catch (err) {
     throw new Error(err);
   }
 };
+
 export default fetchNewsData;
