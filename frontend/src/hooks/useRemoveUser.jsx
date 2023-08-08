4ba1cfc6ad9295a0038e415cea07d3db28f903e1
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-08-08 20:40:35 +0200

    feat: Enhance useRemoveUser to log out user from both frontend and backend

--- frontend/src/hooks/useRemoveUser.jsx
+++ frontend/src/hooks/useRemoveUser.jsx
@@ -1,13 +1,21 @@
 import { useContext } from "react";
 import { WholeAppContext } from "../App";
+import axiosInstance from "../utils/axiosInstance";
 
 const useRemoveUser = () => {
   const { changeUser } = useContext(WholeAppContext);
 
-  const removeUser = () => {
-    changeUser(null);
-    localStorage.removeItem("user");
-    localStorage.removeItem("authToken");
+  const removeUser = async () => {
+    console.log("Remove and logout user.");
+    try {
+      await axiosInstance.post("/api/v1/dj-rest-auth/logout/");
+      changeUser(null);
+      localStorage.removeItem("user");
+      localStorage.removeItem("authToken");
+    } catch (error) {
+      // Handle error
+      console.error("Error while removing user:", error);
+    }
   };
 
   return removeUser;
