3351376605905a9e8cede7ca9c9f8e581a267505
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-12-06 22:55:22 +0100

    fix: fix bugs related to vite deployment behavior

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "",
+  build: {
+    // Ensure that assets are copied to the output directory
+    assetsInlineLimit: 1,
+  },
   plugins: [react()],
   server: {
     // this ensures that the browser opens upon server start
